0f72b1cf429b16000f35ed7b053e7842152f72ec
yoshinori <laskatan@gmail.com>
2023-04-20 02:01:37 +0900

    first commit

---
+++ components/JsonTable/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <table v-if="json">
+    <tbody>
+    <tr v-for="[key, value] in data">
+        <th>{{ key }}</th>
+        <td>
+            <template v-if="Array.isArray(value)">
+                <JsonTable v-for="v in value" :json="v"></JsonTable>
+            </template>
+            <template v-else-if="value !== null && typeof value === 'object'">
+                <JsonTable :json="value"></JsonTable>
+            </template>
+            <template v-else>{{ value }}</template>
+        </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+    props: {
+        json: Object
+    },
+    computed: {
+        data() {
+            return Object.entries(this.json)
+        }
+    }
+}
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <form>
+          <textarea v-model="yamlInput"></textarea>
+      </form>
+
+      <JsonTable :json="jsoned"></JsonTable>
+
+      {{ common }}
+  </div>
+</template>
+<script>
+import jsyaml from 'js-yaml'
+export default {
+    data() {
+        return {
+            yamlInput: ''
+        }
+    },
+    computed: {
+        jsoned() {
+            const doc = jsyaml.load(this.yamlInput, 'utf8')
+            return doc
+        },
+        common() {
+            if (!this.yamled) {
+                return
+            }
+
+            return this.yamled['基本情報']
+        }
+    }
+}
+</script>
